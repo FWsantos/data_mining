d8d554657e7b0bf9b16cde2ddce55287f1b21939
Wesley Santos <fwsantos23@gmail.com>
2021-04-11 11:20:02 -0300

    update

--- src/components/Screens/Home.style.ts
+++ src/components/Screens/Home.style.ts
@@ -4,8 +4,8 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            textAlign: 'center',
             backgroundColor: '#282c34',
+            textAlign: 'center',
             minHeight: '100vh',
             display: 'flex',
             flexDirection: 'column',
@@ -18,7 +18,22 @@ const useStyles = makeStyles((theme: Theme) =>
             fontSize: '100px',
         },
         paperOutput: {
-            // marginTop: '1000px',
+            margin: '10px',
+            // // backgroundColor: '#122122',
+            textAlign: 'center',
+            // height:400,
+            width:400,
+            // minHeight: '100vh',
+            display: 'flex',
+            // flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white',
+            // fontSize: '100px',
+        },
+        inputConfig: {
+            // color: '#ffffff',
+            // backgroundColor: '#ffffff'
         },
     }),
 );
--- src/components/Screens/Home.tsx
+++ src/components/Screens/Home.tsx
@@ -1,17 +1,18 @@
-import { Box, Paper, Typography } from "@material-ui/core";
-import React from "react";
+import { Box, Paper, TextField, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import useStyles from './Home.style';
 
 const Home: React.FC = () => {
     const classes = useStyles();
+
     const brain = require('brain.js/src/index');
-    
-    // provide optional config object (or undefined). Defaults shown.
+ 
+    // // provide optional config object (or undefined). Defaults shown.
     const config = {
         binaryThresh: 0.5,
         hiddenLayers: [3], // array of ints for the sizes of the hidden layers in the network
         activation: 'sigmoid', // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh'],
-        leakyReluAlpha: 0.01, // supported for activation type 'leaky-relu'
+        // leakyReluAlpha: 0.01, // supported for activation type 'leaky-relu'
     };
 
     // create a simple feed forward neural network with backpropagation
@@ -19,17 +20,20 @@ const Home: React.FC = () => {
 
     net.train([
         { input: [0, 0], output: [0] },
-        { input: [0, 1], output: [1] },
-        { input: [1, 0], output: [1] },
-        { input: [1, 1], output: [0] },
+        { input: [0, 1], output: [0] },
+        { input: [1, 0], output: [0] },
+        { input: [1, 1], output: [1] },
     ]);
-
+    
     const output = net.run([1, 0]);
+    
     console.log(output);
     return (
         <Box className={classes.root}>
-            <Typography component='h1' className={classes.texto}> Acho que deu certo </Typography>
-            <Paper className="paperOutput"> {output} </Paper>
+            <Typography component='h1' className={classes.texto}> 
+                XOR Function
+            </Typography>
+            <Paper> {output} </Paper>
         </Box>
     );
 }
